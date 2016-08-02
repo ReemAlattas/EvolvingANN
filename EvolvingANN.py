c303e33cd6274147c596fa528afe1994b1f084c1
ReemAlattas <ralattas@live.com>
2016-08-02 15:27:12 -0400

    update neurons

--- EvolvingANN.py
+++ EvolvingANN.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import random
 import matplotlib as plt
 import math
@@ -7,12 +7,26 @@ numNeurons = 10
 numUpdates = 10
 
 def MatrixCreate(rows, cols):
-    matrix = numpy.zeros(shape=(rows, cols))
+    matrix = np.zeros(shape=(rows, cols))
     return matrix
     
 def MatrixRandomize(v):
     random_m = [[random.random() for y in range(len(v[x]))] for x in range(len(v))]
     return random_m
+    
+def Update (neuronValues, synapses, i):
+    temp = 0
+    sum = 0 
+    for j in range(numNeurons):
+        for k in range(numNeurons):
+            temp = neuronValues[i-1][k] * synapses[j][k]
+            sum = sum + temp
+        if (sum < 0):
+            sum = 0
+        if (sum > 1):
+            sum = 1
+        neuronValues[i][j] = sum        
+    return neuronValues
 
 parent = MatrixCreate(numNeurons, numNeurons)
 parent = MatrixRandomize(parent)
@@ -20,4 +34,9 @@ parent = MatrixRandomize(parent)
 neuronValues = MatrixCreate(numUpdates, numNeurons)
 neuronValues[0] = 0.5
 
-print (neuronValues)
+desiredNeuronValues = np.zeros(10)
+
+for i in range(1, numUpdates):
+    neuronValues = Update (neuronValues, parent, i)
+
+print (desiredNeuronValues)
