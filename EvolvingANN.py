e8b24613ccf4a87ff16bb315ce5a7c6533536c23
ReemAlattas <ralattas@live.com>
2016-08-02 15:08:19 -0400

    create random matrix

--- EvolvingANN.py
+++ EvolvingANN.py
@@ -1,1 +1,19 @@
-#test
+import numpy
+import random
+import matplotlib as plt
+import math
+
+numNeurons = 10
+
+def MatrixCreate(rows, cols):
+    matrix = numpy.zeros(shape=(rows, cols))
+    return matrix
+    
+def MatrixRandomize(v):
+    random_m = [[random.random() for y in range(len(v[x]))] for x in range(len(v))]
+    return random_m
+
+parent = MatrixCreate(numNeurons, numNeurons)
+parent = MatrixRandomize(parent)
+
+print (parent)
